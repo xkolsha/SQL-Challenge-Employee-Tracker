dc35af07239dabc54da929faced09cfa4862b966
xkolsha <aviad85@outlook.com>
2023-07-24 13:57:25 -0300

    added connection with dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,11 @@
 import dotenv from "dotenv";
 import mysql from "mysql2";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "employee_tracker",
+});
