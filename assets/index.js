8f2f64cf2873c17655e64e118daa9ec1c1f92cec
xkolsha <aviad85@outlook.com>
2023-07-24 18:01:39 -0300

    added index.js for prompts

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+import connection from "../db/connection.";
+
+async function mainMenu() {
+  const answer = await inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Exit",
+    ],
+  });
+}
